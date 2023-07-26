f76dfc9648ea2516e76c25cab5bef204f1d54ff4
Ifeoluwa <ifel1939@gmail.com>
2023-07-26 08:36:01 +0000

    instance

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,6 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+export default function getResponseFromAPI() {
+   return new Promise((resolve, reject) => {
+    if (!success) reject(new Error('The fake API is not working currently'));
+    resolve("success");
+  });;
 }
